b0cca38cf0b50925bf5d7aa733d7657beeca0d52
Oscar Spencer <oscar@grain-lang.org>
2021-09-07 21:13:02 -0400

    feat: v0.4.2

--- Casks/grain.rb
+++ Casks/grain.rb
@@ -1,6 +1,6 @@
 cask "grain" do
-  version "0.4.1"
-  sha256 "732675efa0f3646bd047d1ec4c37cb87b79f5204298176a61c9f5e2004854493"
+  version "0.4.2"
+  sha256 "ede33c6e1f63a73f3bbd0506d476749ab2d705ca5e3e28eca290dafedad49649"
 
   url "https://github.com/grain-lang/grain/releases/download/grain-v#{version}/grain-mac-x64", verified: "https://github.com/grain-lang/grain/"
   name "The Grain Programming Language"
