0a86ebf718c99fb18a1aa07194cb83952e9b407a
Blaine Bublitz <blaine.bublitz@gmail.com>
2022-05-30 18:34:33 -0700

    Point at my fork

--- Casks/grain.rb
+++ Casks/grain.rb
@@ -2,7 +2,7 @@ cask "grain" do
   version "0.4.7"
   sha256 "208400874321305b9ff35a0379f1e78ba20632538cdf3921d43810612b239e70"
 
-  url "https://github.com/grain-lang/grain/releases/download/grain-v#{version}/grain-mac-x64", verified: "https://github.com/grain-lang/grain/"
+  url "https://github.com/phated/grain/releases/download/grain-v#{version}/grain-mac-x64", verified: "https://github.com/phated/grain/"
   name "The Grain Programming Language"
   desc "Packaged Grain binary, including the compiler, runtime & stdlib"
   homepage "https://grain-lang.org/"
